f56b49dde0896dbdc74f9775cdb88aa35f4c227b
glennog <ianglennon@gmail.com>
2020-12-19 20:06:56 +0000

    Package: snowman   Class: RGBSnowman   Class: State(Enum)             STOPPED             SWAP_STANDARD             FADE_STANDARD

--- snowman.py
+++ snowman.py
@@ -1,102 +1,47 @@
+from snowman import RGBSnowman, State
 import RPi.GPIO as GPIO
-import time
-import random
-import sys
+import signal
 from datetime import datetime
 
-pinButton = 21
-pinRed = 19
-pinGreen = 17
-pinBlue = 13
 
-color_palette = [
-	(0,0,255),
-	(179,12,0),
-	(220,42,61),
-	(13,66,239),
-	(0,44,179),
-	(13,1,89),
-	(255,0,0),
-	(255,120,120),
-	(255,255,255),
-	(116,129,214),
-	(55,41,139)
-]
-
-
-def setup():
-	global pwmRed, pwmBlue, pwmGreen
-	GPIO.setmode(GPIO.BCM)
-
-	GPIO.setup(pinRed, GPIO.OUT)
-	GPIO.setup(pinGreen, GPIO.OUT)
-	GPIO.setup(pinBlue, GPIO.OUT)
-	GPIO.setup(pinButton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-	GPIO.output(pinRed, GPIO.HIGH)
-	GPIO.output(pinGreen, GPIO.HIGH)
-	GPIO.output(pinBlue, GPIO.HIGH)
-	pwmRed = GPIO.PWM(pinRed, 2000)
-	pwmGreen = GPIO.PWM(pinGreen, 2000)
-	pwmBlue = GPIO.PWM(pinBlue, 2000)
-
-	pwmRed.start(0)
-	pwmGreen.start(0)
-	pwmBlue.start(0)
-
-
-def setColour(red: int, green: int, blue: int):
-	r = (red/255)*100
-	g = (green/255)*100
-	b = (blue/255)*100
-	pwmRed.ChangeDutyCycle(r)
-	pwmGreen.ChangeDutyCycle(g)
-	pwmBlue.ChangeDutyCycle(b)
-	#print('r,b,g = %d,%d,%d') % (r,b,g)
-
-
-def loop(timer: int = 3):
-	print('Starting up')
-	current_colour = None
-	running = True
-	starttime = int(datetime.now().timestamp())-timer
-	buttontime = int(datetime.now().timestamp())
-	buttontimeout = 2
+def loop(snowman: RGBSnowman):
+	start_time = int(datetime.now().timestamp()) - snowman.pause_time
+	button_time = int(datetime.now().timestamp())
+	button_timeout = 2
 	while True:
-		if GPIO.input(pinButton) == GPIO.LOW:
-			if int(datetime.now().timestamp()) >= buttontime+buttontimeout:
-				running = not running
-				buttontime = int(datetime.now().timestamp())
-				print('Swapping state : %s' % running)
-		
-		if running:
-			tnow = int(datetime.now().timestamp())
-			if int(tnow) >= starttime+timer:
-				new_colour = random.randint(0,len(color_palette)-1)
-				if current_colour is None or current_colour != new_colour:
-					setColour(color_palette[new_colour][0], 
-						color_palette[new_colour][1], 
-						color_palette[new_colour][2])
-					current_colour = new_colour
-					starttime = int(datetime.now().timestamp())
-		else:
-			setColour(0,0,0)
-			
+		if GPIO.input(snowman.pin_button) == GPIO.LOW:
+			if int(datetime.now().timestamp()) >= button_time + button_timeout:
+				snowman.button_press()
+				button_time = int(datetime.now().timestamp())
 
-
-def destroy():
-	print('Stopping')
-	pwmRed.stop()
-	pwmBlue.stop()
-	pwmGreen.stop()
-	GPIO.cleanup()
+		if snowman.state != State.STOPPED:
+			t_now = int(datetime.now().timestamp())
+			if int(t_now) >= start_time + snowman.pause_time:
+				snowman.change_colour(with_set=True)
+				start_time = int(datetime.now().timestamp())
 
 
 if __name__ == '__main__':
-	timer = 3
-	setup()
+	sman = RGBSnowman(pin_button=21, pin_red=19, pin_green=17, pin_blue=13, fade_time=1, pause_time=1)
+	sman.start()
+
+	def signal_shutdown(signum, frame):
+		sman.shutdown()
+
+	def signal_status_change(signum, frame):
+		sman.button_press()
+
+	def signal_colour_change(signum, frame):
+		sman.change_colour(with_set=True)
+
+	signal.signal(signal.SIGHUP, signal_colour_change)
+	signal.signal(signal.SIGBREAK, signal_shutdown)
+	signal.signal(signal.SIGKILL, signal_shutdown)
+	signal.signal(signal.SIGSTOP, signal_shutdown)
+	signal.signal(signal.SIGUSR1, signal_status_change)
+
 	try:
-		loop(timer=timer)
+		loop(snowman=sman)
 	except KeyboardInterrupt:
-		destroy()
+		sman.shutdown()
 
---
+++ snowman/__init__.py
@@ -0,0 +1,130 @@
+import RPi.GPIO as GPIO
+import time
+import random
+from enum import Enum
+
+
+class State(Enum):
+    STOPPED = 1
+    SWAP_STANDARD = 2
+    FADE_STANDARD = 3
+
+
+class RGBSnowman:
+    pin_button: int
+    pin_red: int
+    pin_green: int
+    pin_blue: int
+    fade_time: int
+    pause_time: int
+    pwm_red: GPIO.PWM
+    pwm_green: GPIO.PWM
+    pwm_blue: GPIO.PWM
+    state: State
+    current_colour: tuple(int, int, int)
+
+    standard_palette = [
+        (0, 0, 255),
+        (179, 12, 0),
+        (220, 42, 61),
+        (13, 66, 239),
+        (0, 44, 179),
+        (13, 1, 89),
+        (255, 0, 0),
+        (255, 120, 120),
+        (255, 255, 255),
+        (116, 129, 214),
+        (55, 41, 139)
+    ]
+
+    def __init__(self, pin_button: int = None, pin_red: int = None, pin_green: int = None, pin_blue: int = None,
+                 fade_time: int = 1, pause_time: int = 1):
+        if pin_red:
+            self.pin_red = pin_red
+        if pin_button:
+            self.pin_button = pin_button
+        if pin_green:
+            self.pin_green = pin_green
+        if pin_blue:
+            self.pin_blue = pin_blue
+
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setup(self.pin_red, GPIO.OUT)
+        GPIO.setup(self.pin_green, GPIO.OUT)
+        GPIO.setup(self.pin_blue, GPIO.OUT)
+        GPIO.setup(self.pin_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        GPIO.output(self.pin_red, GPIO.HIGH)
+        GPIO.output(self.pin_green, GPIO.HIGH)
+        GPIO.output(self.pin_blue, GPIO.HIGH)
+        self.pwm_red = GPIO.PWM(self.pin_red, 2000)
+        self.pwm_green = GPIO.PWM(self.pin_green, 2000)
+        self.pwm_blue = GPIO.PWM(self.pin_blue, 2000)
+        self.state = State.STOPPED
+        self.current_colour = (0, 0, 0)
+        self.fade_time = fade_time
+        self.pause_time = pause_time
+
+    def change_colour(self, palette: list = None, with_set = False):
+        if palette is None:
+            palette = self.standard_palette
+        new_colour_idx = random.randint(0, len(palette)-1)
+        new_colour = palette[new_colour_idx]
+        if self.current_colour is None:
+            self.current_colour = (0, 0, 0)
+        if self.current_colour != new_colour:
+            if with_set:
+                if self.state == State.SWAP_STANDARD:
+                    self.set_colour(new_colour)
+                elif self.state == State.FADE_STANDARD:
+                    self.fade_colour(old_colour=self.current_colour, new_colour=new_colour)
+            self.current_colour = new_colour
+            return
+        else:
+            self.change_colour(palette=palette, with_set=with_set)
+
+    def fade_colour(self, old_colour: tuple, new_colour: tuple):
+        fade_time = self.fade_time
+        slice_time = fade_time / 1000
+        r_diff = (old_colour[0] - new_colour[0]) / slice_time
+        g_diff = (old_colour[1] - new_colour[1]) / slice_time
+        b_diff = (old_colour[2] - new_colour[2]) / slice_time
+        for i in range(start=0, stop=1000, step=1):
+            i_colour = (r_diff * i, g_diff * i, b_diff * i)
+            self.set_colour(i_colour)
+            time.sleep(slice_time)
+        self.set_colour(new_colour)
+        return new_colour
+
+    def set_colour(self, colour: tuple):
+        r = (colour[0] / 255) * 100
+        g = (colour[1] / 255) * 100
+        b = (colour[2] / 255) * 100
+        self.pwm_red.ChangeDutyCycle(r)
+        self.pwm_green.ChangeDutyCycle(g)
+        self.pwm_blue.ChangeDutyCycle(b)
+
+    def button_press(self):
+        if self.state.value() == len(State):
+            self.state = State(1)
+        else:
+            self.state = State(self.state.value() + 1)
+
+        if self.state == State.STOPPED:
+            self.set_colour((0, 0, 0))
+            self.stop()
+
+    def start(self):
+        self.pwm_red.start(0)
+        self.pwm_green.start(0)
+        self.pwm_blue.start(0)
+
+    def stop(self):
+        self.pwm_red.stop()
+        self.pwm_blue.stop()
+        self.pwm_green.stop()
+
+    def shutdown(self):
+        self.stop()
+        GPIO.cleanup()
